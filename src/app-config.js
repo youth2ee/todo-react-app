08358f0260897a651a04e7ee619ec6bcd05dbd4e
youth2ee <sprirealee@gmail.com>
2022-05-19 02:06:48 +0900

    220519

--- src/app-config.js
+++ src/app-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === "localhost") {
   backendHost = "http://localhost:8080";
 } else {
-  backendHost = "http://prod-todo-backend2222.us-west-2.elasticbeanstalk.com";
+  //backendHost = "http://prod-todo-backend2222.us-west-2.elasticbeanstalk.com";
   //backendHost = "http://prod-todo-frontend.us-west-2.elasticbeanstalk.com";
 }
 
